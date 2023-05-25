4f549172df39e652e1f707c9bbf5361ea6031e95
amediTechWizard <amukoahdavid@gmail.com>
2023-05-25 17:24:38 +0300

    user rooms count - Updated user rooms count on profile

--- base/views.py
+++ base/views.py
@@ -139,9 +139,10 @@ def updateRoom(request, pk):
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
+    user_rooms = rooms.count()
     room_messages = user.message_set.all()
     topics = Topic.objects.all()
-    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    context = {'user': user, 'rooms': rooms, 'user_rooms': user_rooms, 'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
